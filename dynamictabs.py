531261e38b49ba41cc076d9edd5fd6260c3cc049
zhiayang <zhiayang@gmail.com>
2019-09-24 02:39:53 +0800

    ok second

--- dynamictabs.py
+++ dynamictabs.py
@@ -5,21 +5,32 @@ import math
 from functools import reduce
 
 class DynamicTabCommand(sublime_plugin.TextCommand):
-	def get_first_whitespace(self, edit, region):
-		return next(i for i, j in enumerate(self.view.substr(self.view.line(region))) if j.strip())
+	def get_first_whitespace(self, line):
+		for i in range(0, len(line)):
+			if line[i] != '\t' and line[i] != ' ':
+				return i
+		return -1
+
+	def get_first_whitespace_in_region(self, v, region):
+		line = v.substr(v.line(region))
+		return self.get_first_whitespace(line)
 
 	def run(self, edit):
-		v = self.view
+		v           = self.view
+		tabSize     = v.settings().get("tab_size")
+		usingSpaces = v.settings().get("translate_tabs_to_spaces")
+
 		for s in v.sel():
+			wasEmpty = True
 			if not s.empty():
+				wasEmpty = False
 				v.erase(edit, s)
 
-			firstWS  = self.get_first_whitespace(edit, s)
 			thisSel  = v.rowcol(s.begin())[1]
 			thisLine = v.substr(v.line(s))
-			tabSize  = v.settings().get("tab_size")
+			firstWS  = self.get_first_whitespace_in_region(v, s)
 
-			if thisSel > firstWS:
+			if firstWS != -1 and thisSel > firstWS:
 				# ok... we need to calculate the next tabstop.
 				# copied from https://github.com/zhiayang/vscode-tabindentspacealign/blob/master/src/extension.ts#L102
 				# ie myself
@@ -28,14 +39,23 @@ class DynamicTabCommand(sublime_plugin.TextCommand):
 				for i in range(0, thisSel):
 					cursor += ((cursor % tabSize) if thisLine[i] == '\t' else 1)
 
-				finalpos = math.ceil((thisSel + 1) / tabSize) * tabSize;
-				v.insert(edit, s.begin(), (" " * (finalpos - thisSel)))
+				finalpos = math.ceil((cursor+1) / tabSize) * tabSize;
+				v.insert(edit, s.begin(), (" " * (finalpos - cursor)))
 
 			else:
-				if v.settings().get("translate_tabs_to_spaces"):
-					v.insert(edit, s.begin(), " " * tabSize)
+				mult = 1
+				if wasEmpty and v.rowcol(s.begin())[0] > 0:
+					prevLine = v.substr(v.line(v.text_point(v.rowcol(s.begin())[0] - 1, 0)))
+					ws = self.get_first_whitespace(prevLine)
+					if thisSel < ws and ws != -1:
+						mult = (ws / tabSize) if usingSpaces else ws
+						if prevLine[-1] in ("{", ":", ","):
+							mult += 1
+
+				if usingSpaces:
+					v.insert(edit, s.begin(), " " * tabSize * mult)
 				else:
-					v.insert(edit, s.begin(), "\t")
+					v.insert(edit, s.begin(), "\t" * mult)
 
 
 
